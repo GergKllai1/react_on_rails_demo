311ffbc0050533695ea6aae358cdb2c6d2d9c371
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-27 21:57:40 +0100

    Adds the method to post the data in the database and also set the state of the items array

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -4,22 +4,48 @@ class Body extends React.Component {
     this.state = {
       items: []
     };
-    this.handleFormSubmit = this.handleFormSubmit.bind(this)
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.addNewItem = this.addNewItem.bind(this);
   }
   handleFormSubmit(name, description) {
-    console.log('form works!')
-  };
+    let body = JSON.stringify({
+      item: { name: name, description: description }
+    });
+    fetch("http://localhost:3000/api/v1/items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: body
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(item => {
+        this.addNewItem(item);
+      });
+  }
+
+  addNewItem(item) {
+    this.setState({
+      items: this.state.items.concat(item)
+    });
+  }
 
   componentDidMount() {
-    fetch('/api/v1/items.json')
-      .then((response) => {return response.json()})
-      .then((data) => {this.setState({ items: data }) });
+    fetch("/api/v1/items.json")
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        this.setState({ items: data });
+      });
   }
   render() {
     return (
       <div>
-        <NewItem handleFormSubmit={this.handleFormSubmit}/>
-        <AllItems items={this.state.items}/>
+        <NewItem handleFormSubmit={this.handleFormSubmit} />
+        <AllItems items={this.state.items} />
       </div>
     );
   }
