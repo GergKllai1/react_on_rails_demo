b82f6d6d41e60a55e4e7661d74035078d371e3a7
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-27 19:16:43 +0100

    Refactors components, makes the call in the parent body component and passes down the infomration using props to the all_items controller

--- app/assets/javascripts/components/_all_items.js.jsx
+++ app/assets/javascripts/components/_all_items.js.jsx
@@ -1,27 +1,12 @@
-class AllItems extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: []
-    };
-  }
+const AllItems = props => {
+  const items = props.items.map(item => {
+    return (
+      <div key={item.id}>
+        <h3>{item.name}</h3>
+        <p>{item.description}</p>
+      </div>
+    );
+  });
 
-  componentDidMount() {
-    fetch('/api/v1/items.json')
-      .then((response) => {return response.json()})
-      .then((data) => {this.setState({ items: data }) });
-  }
-
-  render() {
-    const items = this.state.items.map(item => {
-      return (
-        <div key={ item.id }>
-          <h3>{item.name}</h3>
-          <p>{item.description}</p>
-        </div>
-      );
-    });
-
-    return <div>{items}</div>;
-  }
-}
+  return <div>{items}</div>;
+};
--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -1,9 +1,21 @@
 class Body extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api/v1/items.json')
+      .then((response) => {return response.json()})
+      .then((data) => {this.setState({ items: data }) });
+  }
   render() {
     return (
       <div>
         <NewItem />
-        <AllItems />
+        <AllItems items={this.state.items}/>
       </div>
     );
   }
